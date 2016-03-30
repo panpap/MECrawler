d0540a48ce450d8f8a23b5d2ec5c39b0cda7e592
panpap <panpap@gmx.com>
2016-03-30 18:50:30 +0200

    =

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
+echo "Installing needed resources..."
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt-get update
 sudo apt-get install -f
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm -f google-chrome-stable_current_amd64.deb
 sudo apt-get install xvfb ruby openjdk-7-jdk
+echo "Setting cronjob..."
+echo "0 */4   * * *   cd $PWD && ruby crawlList.rb -e extensions/adblockplus-1.11.0.1580.crx -f $1 -t Regular4G" > crontabFile
+crontab crontabFile
+
