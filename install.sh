2f30a86df3983e19782cdec5a8887af24623c687
panpap <panpap@gmx.com>
2016-03-31 20:17:48 +0200

    =

--- install.sh
+++ install.sh
@@ -10,6 +10,6 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm -f google-chrome-stable_current_amd64.deb
 sudo apt-get install xvfb ruby openjdk-7-jdk
 echo "Setting cronjob..."
-echo "0 */4   * * *   cd $PWD && ruby crawlList.rb -e extensions/adblockplus-1.11.0.1580.crx -f $1 -r 5 -t Regular4G &> $1.out" > crontabFile
+echo "0 */4   * * *   cd $PWD && ruby crawlList.rb -e extensions/adblockplus-1.11.0.1580.crx -f $1 -r 5 -t Regular4G > $1.out 2>&1" > crontabFile
 crontab crontabFile
 Xvfb :1 -screen 5 1024x768x8 &
