ced63a58faacffc3c7f762a59e6b03e210844365
panpap <panpap@gmx.com>
2016-04-12 11:18:13 +0300

    =

--- src/ProxifiedChrome.java
+++ src/ProxifiedChrome.java
@@ -2,6 +2,7 @@ package mobileEmulator;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
@@ -10,6 +11,7 @@ import java.util.concurrent.TimeUnit;
 import net.lightbody.bmp.BrowserMobProxyServer;
 import net.lightbody.bmp.client.ClientUtil;
 import net.lightbody.bmp.core.har.Har;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
@@ -18,16 +20,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.SessionNotFoundException;
+import org.openqa.selenium.remote.UnreachableBrowserException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ProxifiedChrome{
     private static final int SERVER_PORT=4444;
-    private static final long WAITTIME=2; //minutes
+    private static final long WAITTIME=60000; //millisec
     private final Proxy proxy;	
     private BrowserMobProxyServer server;
     private final String resultsDir;
     private final String throttleType;
+    final static Logger logger = LoggerFactory.getLogger(ProxifiedChrome.class);
 
-    public ProxifiedChrome(String throttleType,String dir) throws IOException, InterruptedException{
+    public ProxifiedChrome(String throttleType,String dir) throws IOException{
         this.resultsDir=dir;
         this.throttleType=throttleType;
         Integer[] throttle=translateThrottling();
@@ -35,10 +42,11 @@ public class ProxifiedChrome{
     }
     
     public void close(){
+        System.out.println("I'm closing server");
         this.server.stop();
     }
 
-    public void runTest(String myurl,String device,String extension,int replay) throws IOException, InterruptedException{
+    public void runTest(String myurl,String device,String extension,int replay,Boolean caching) throws IOException{
         String print="";String domain;
         if (device==null)
             print+=" without mobile emulation";
@@ -46,8 +54,10 @@ public class ProxifiedChrome{
             print+=" with mobile emulation ("+device+")";
         if (extension==null)
             print+=" without extension";
-        else
-            print+=" with extension";
+        else{
+            String[] tmp=extension.split("/");
+            print+=" with "+tmp[tmp.length-1]+" extension";
+        }
         System.out.println("Crawling "+myurl+print+" "+replay);
         if (myurl.contains("://")){
             domain=myurl.split("://")[1];
@@ -57,7 +67,7 @@ public class ProxifiedChrome{
         }
 	// create a new HAR with the label: domain
         this.server.newHar(domain);
- 	openBrowser(myurl,device,extension);
+ 	openBrowser(myurl,device,extension,caching);
         // get the HAR data
 	storeHarFile(domain,device,extension,replay);
     }
@@ -83,20 +93,67 @@ public class ProxifiedChrome{
         har.writeTo(harFile);
     }
 
-    private void openBrowser(String myurl,String device,String extension) throws InterruptedException, IOException{
+    private void openBrowser(String myurl,String device,String extension,Boolean caching) throws IOException{
         long startTime = System.currentTimeMillis();
-        WebDriver driver = startChromeDriver(this.proxy,device,extension);
-        driver.manage().timeouts().pageLoadTimeout(WAITTIME,TimeUnit.MINUTES);
-        //server.blacklistRequests(this.extension.split("\"), 200); 
+        WebDriver driver = null;
         try{
-            driver.get(myurl);
-        }catch(TimeoutException e){
-            dumpToErrorLog("There was a Timeout for: "+myurl,e);
+          driver = startChromeDriver(this.proxy,device,extension,caching);
+     //   driver.manage().timeouts().pageLoadTimeout(WAITTIME,TimeUnit.MILLISECONDS);
+   /*     if(extension!=null){
+            String[] tmp = extension.split("/");
+            String temp=tmp[tmp.length-1];
+            if(temp.contains("-"))
+                temp=temp.split("-")[0];
+            if(temp.contains("_"))
+                temp=temp.split("_")[0];
+            System.out.println("Blacklisting "+"http://.*\\."+temp+".*");
+            this.server.blacklistRequests("http://.*\\."+temp+".*", 200); 
+        }*/
+        
+            if(caching==false)
+                driver.manage().deleteAllCookies();
+            safeFetch(myurl,driver);
         }catch(WebDriverException e){
             dumpToErrorLog("There was an exception for: "+myurl,e);
+        } finally {
+            try{
+                if (driver!=null){
+                    driver.close();
+                    driver.quit();
+                }
+            }catch(SessionNotFoundException e){}
         }
-        driver.quit();
         System.out.println("Elapsed time "+(System.currentTimeMillis() - startTime)+" ms");
+    }
+    
+    private void safeFetch(String url, final WebDriver driver){
+        Thread t = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                driver.get(Thread.currentThread().getName());
+            }
+        }, url);
+        t.start();
+        try
+        {
+          //  t.join(WAITTIME);
+            Thread.sleep(WAITTIME);
+        }
+        catch (InterruptedException e)
+        { // ignore
+        }
+        if (t.isAlive())
+        { // Thread still alive, we need to abort
+          System.out.println("RAISE timeout on loading page " + url);
+          t.interrupt();
+ //         closeWindow(driver);
+  //        System.out.println("JSCLOSED");
+          driver.close();
+          System.out.println("driverCLOSED");
+          driver.quit();
+          System.out.println("driverQUIT " + url);
+        }
     }
     
     private ChromeOptions mobileEmulation(String device,ChromeOptions chromeOptions){
@@ -106,7 +163,7 @@ public class ProxifiedChrome{
         return chromeOptions;
     }
             
-    private ChromeDriver startChromeDriver(Proxy proxy,String device,String extension) throws IOException{
+    private ChromeDriver startChromeDriver(Proxy proxy,String device,String extension,Boolean caching) throws IOException{
         //Selenium
         ChromeOptions chromeOptions = new ChromeOptions();    //chromeOptions.put("binary", "/usr/bin/google-chrome");
         //chromeOptions.addArguments("--incognito");
@@ -115,6 +172,10 @@ public class ProxifiedChrome{
         if (device!=null)
             chromeOptions=mobileEmulation(device,chromeOptions); //mobile emulation
         DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        //NO CACHING
+        if (caching==false)
+            chromeOptions.addArguments("--disable-application-cache");
+        //chromeOptions.addArguments("--no-sandbox");
         capabilities.setCapability(CapabilityType.PROXY, proxy);
         capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);        
         return new ChromeDriver(capabilities);
@@ -203,8 +264,16 @@ public class ProxifiedChrome{
 
     private void dumpToErrorLog(String string,Exception e) throws FileNotFoundException {
         System.err.println("ERROR: "+string);
-        try(  PrintWriter out = new PrintWriter(this.resultsDir+"/errorLog.log")){
-            out.println("\n"+string+"\n"+e);
-        }
+        PrintWriter out = new PrintWriter(new FileOutputStream(new File(this.resultsDir+"/errorLog.log"), true));
+        out.append("\n"+string+"\n"+e+"\n");
+        out.flush();
+        out.close();
+    }
+
+    private void closeWindow(WebDriver driver) {
+        System.out.println("Force closing");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        String script = "window.onbeforeunload = null;" + "window.close();";
+        js.executeScript(script);
     }
 }
--- src/mobileEmulatorCrawler.java
+++ src/mobileEmulatorCrawler.java
@@ -3,48 +3,78 @@ package mobileEmulator;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import java.io.BufferedReader;
-import java.io.IOException; 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
         
 public class mobileEmulatorCrawler {
-    private static int replays=1;
     
-    private static void looper(String file,String throttle,String device,String extension,String resultsDir) throws IOException, InterruptedException{	
-        try{
-            ProxifiedChrome proxySel=new ProxifiedChrome(throttle,resultsDir);
-            InputStream fis = new FileInputStream(file);
-            InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
-            BufferedReader br = new BufferedReader(isr);
-            String url;
-            while ((url = br.readLine()) != null) {
-                for(int i=0; i<replays;i++){
-                    //mobile+no extension
-                    if (extension!=null)
-                        proxySel.runTest(url,device,null,i);
-                    //mobile+extension
-                    proxySel.runTest(url,device,extension,i);
-                    if(device!=null){
-                        //no mobile+no extension
-                        if (extension!=null) 
-                            proxySel.runTest(url,null,null,i);
-                        //no mobile+extension
-                        proxySel.runTest(url,null,extension,i);
-                    }
+    
+    private static void looper(int run, String file, String throttle, String device, String extension, String absolutePath, Boolean caching) throws IOException, InterruptedException {
+       Worker worker[] = new Worker[5];
+       int k=0;
+        //no mobile+no extension (vanilla)
+        //runList(i,file,throttle,null,null,absolutePath,caching);
+        worker[k] = new Worker(file,null,null,run,caching,throttle,absolutePath);
+        worker[k].start();
+//worker[k].join();
+        k++;
+        //mobile+extension
+        if (extension!=null){
+           // if (extension.contains(".crx")){
+                //runList(i,file,throttle,device,extension,absolutePath,caching); 
+                worker[k] = new Worker(file,device,extension,run,caching,throttle,absolutePath);
+                worker[k].start();k++;
+//worker[k].join();
+
+                if(device!=null){//no mobile+extension
+            //        runList(i,file,throttle,null,extension,absolutePath,caching);
+                    worker[k] = new Worker(file,null,extension,run,caching,throttle,absolutePath);
+                    worker[k].start();k++;
+//worker[k].join();
                 }
+           /* }else{
+         //       allExtensions(i,file,throttle,device,extension,absolutePath,caching);
+                worker[k] = new Worker(file,device,extension,run,caching,throttle,absolutePath);
+                worker[k].start();k++;
+                if(device!=null)//no mobile+extension
+          //          allExtensions(i,file,throttle,null,extension,absolutePath,caching);
+                    worker[k] = new Worker(file,null,extension,run,caching,throttle,absolutePath);
+                    worker[k].start();k++;
+            }*/
+        }else{   //mobile+no extension 
+            if(device!=null){
+               // runList(i,file,throttle,device,null,absolutePath,caching);
+                worker[k] = new Worker(file,device,null,run,caching,throttle,absolutePath);
+                worker[k].start();k++;
+//worker[k].join();
+
             }
-            proxySel.close();
-        }      
-        catch (FileNotFoundException ex) {
-            System.err.println("ERROR: Could not find file "+file);
-            System.exit(-1);
+        }
+        for(int i=0;i<k;i++){
+            try
+            {
+                worker[i].join(60000);
+            }
+            catch (InterruptedException e)
+            { // ignore
+            }
+            if (worker[i].isAlive())
+            { // Thread still alive, we need to abort
+                System.out.println("Kill me "+i);
+                worker[i].interrupt(); 
+            }
         }
     }
-    
+
     public static void main(String []args) throws Exception {
         Settings settings = new Settings();
         new JCommander(settings, args);
@@ -56,13 +86,95 @@ public class mobileEmulatorCrawler {
             filename=settings.file.split("/")[1];
         if (filename.contains("."))
             filename=filename.split("\\.")[0];
-        System.out.println(filename);
-        File dir = new File(cwd+"/"+filename+"_results");
+        Timestamp currentTimestamp = new java.sql.Timestamp(Calendar.getInstance().getTime().getTime());
+        File dir = new File(cwd+"/"+filename+"_"+currentTimestamp+"_results_");
         boolean successful = dir.mkdir();
-        System.setProperty("webdriver.chrome.driver", cwd+"/bin/chromedriver");                
-        looper(settings.file,settings.throttle,settings.device,settings.extension,dir.getAbsolutePath());
+        System.setProperty("webdriver.chrome.driver", cwd+"/bin/chromedriver"); 
+        int replays=1;
+        Boolean caching=true;
+        if (settings.replays!=null)
+            replays=settings.replays;
+        if (settings.replays!=null)
+            replays=settings.replays;
+        if (settings.caching!=null)
+            caching=Boolean.parseBoolean(settings.caching);
+        System.out.println("Input Given:\n--------------\nReplays: "+replays);
+        for(int i=0; i<replays;i++)
+            looper(i,settings.file,settings.throttle,settings.device,settings.extension,dir.getAbsolutePath(),caching);
     }
 }
+
+class Worker extends Thread{
+    private final String file, device, extension,  throttle, resultsDir;
+    private final int run;
+    private final Boolean caching;
+    
+    public Worker(String file,String device,String extension,int run,Boolean caching,String throttle,String resultsDir)
+    {
+        this.file=file;
+        this.device=device;
+        this.extension=extension; 
+        this.throttle=throttle;
+        this.resultsDir=resultsDir;
+        this.run=run;
+        this.caching=caching;
+    }
+    
+    @Override
+    public void run()
+    {
+        try {
+            System.out.println("New Thread");
+            if ((this.extension==null) || (this.extension.contains(".crx")))
+                runList(this.run,this.file,this.throttle,this.device,this.extension,this.resultsDir,this.caching);
+            else
+                allExtensions(this.run,this.file,this.throttle,this.device,this.extension,this.resultsDir,this.caching);
+        } catch (IOException ex) {
+            Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+ 
+    private static void runList(int run, String file,String throttle,String device,String extension,String resultsDir, Boolean caching) throws IOException, InterruptedException{	
+        System.out.println("File: "+file+"\nExtention: "+ extension+"\nDevice: "+device+"\nThrottle: "+throttle+"\n");
+        try{    
+            InputStream fis = new FileInputStream(file);
+            InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+            BufferedReader br = new BufferedReader(isr);
+            String url;   
+            while ((url = br.readLine()) != null) {
+                ProxifiedChrome proxySel=new ProxifiedChrome(throttle,resultsDir);
+                proxySel.runTest(url,device,extension,run,caching);
+                Thread.sleep(500);
+                proxySel.close();
+            }
+        }
+        catch (FileNotFoundException ex) {
+            System.err.println("ERROR: Could not find file "+file);
+            System.exit(-1);
+        }
+    }
+    
+    private static void allExtensions(int run, String file, String throttle, String device, String extensionPath, String absolutePath, Boolean caching) throws IOException, InterruptedException{
+        File folder = new File(extensionPath);
+        File[] listOfFiles = folder.listFiles();        
+        System.out.println("Load all "+listOfFiles.length+" extensions of "+extensionPath);
+        if (listOfFiles.length>0){
+            for (int i = 0; i < listOfFiles.length; i++) {
+                if (listOfFiles[i].isFile()){
+                    //System.out.println(listOfFiles[i].getPath());
+                    runList(run,file,throttle,device,listOfFiles[i].getPath(),absolutePath,caching);
+                }
+            }
+        }
+        else{
+           System.err.println("NO EXTENSIONS WERE FOUND");
+           System.exit(-1);
+        }
+    }
+}
+
 
 class Settings {
     @Parameter(names = "-file", description = "URL list to crawl", required = true)
@@ -79,4 +191,10 @@ class Settings {
     
     @Parameter(names = "-dir", description = "Change folder with required resourcers", required = false)
     public String dir;
+    
+    @Parameter(names = "-r", description = "Number of back-to-back reruns", required = false)
+    public Integer replays;
+    
+    @Parameter(names = "-c", description = "Use Caching and cookies", required = false)
+    public String caching;
 }
